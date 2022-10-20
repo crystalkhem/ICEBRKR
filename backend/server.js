69220df945359cdbfaa062f1b9b7e662dfa2f909
Gowtham Ravella <gowthamkrishnaravella@gmail.com>
2022-10-20 19:18:08 -0400

    Added Folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,30 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
+import express from "express";
+import mongoose from "mongoose";    
+import dotenv from "dotenv";
+import cors from "cors";
 
-const app = express()
+dotenv.config()
+const app = express();
 app.use(express.json())
 
 
+// Accepts json data in req.body
+app.use(express.json());
+// app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-//connect to data base
-mongoose.connect(process.env.URI)
-    .then(() => {
-        app.listen(process.env.PORT, () => console.log('sucessfully connected to mongoDB'))
-    }) .catch((error) => console.log(error))
+app.get('/', (req, res) => {
+    res.json({msg: 'Welcome to ICRBRKR'})
+})
 
+
+// Custom Error Handlers Through Middleware
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
