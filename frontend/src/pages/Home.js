885da566c2bac9083d5c546f5dc4fa509beaa13b
crystalkhem <47156291+crystalkhem@users.noreply.github.com>
2022-10-21 15:48:26 -0400

    working on category buttons
    working towards category selection to deselect and select with buttons. not finished.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,16 +1,46 @@
-// import { useEffect, useState } from 'react'
+import { useState } from 'react'
 // import { BrowserRouter as Router, Link } from 'react-router-dom'
 const Home = () => {
+
+    // const checkboxes = document.querySelectorAll('.checkbox')
+
+    // const handleCheckbox = event => {
+    //     console.log(event.target.value)
+    //      console.log('this is all of them:' + checkboxes)
+        
+    // }
+
+    const [cats, updateCats] = useState([])
+
+    const  onClick = (event) => {
+        if (event.target.checked) {
+        updateCats([...cats, event.target.value])
+        } else if (!event.target.checked && cats.length === 1) {
+            updateCats([])
+        } else if (!event.target.checked) {
+            updateCats([cats.splice(cats.indexOf(event.target.value))])
+        }
+    }
+
 
     return (
         <div className="categories">
             <h2>categories</h2>
-            sports<input type="checkbox" name="layer one" value="sports" /><br />
-            music<input type="checkbox" name="layerone" value="music" /><br />
-            tv shows<input type="checkbox" name="layerone" value="tv shows" /><br />
-            animals<input type="checkbox" name="layerone" value="animals" />
-        </div>
+            <div className="checkbox">music<input type="checkbox" onChange={onClick} name="layerone" value="music" /><br /></div>
+            
+            <div className="checkbox">sports<input type="checkbox" onChange={onClick} name="layerone" value="sports" /><br /></div>
+            
+            <div className="checkbox">movies<input type="checkbox" onChange={onClick} name="layerone" value="movies" /><br /></div>
+            
+            <br />
+            <br />
+            <p><b>categories selected:</b> { cats.join(' ') } </p>
+
+
+</div>
+
     )
-}
+    }
+
 
 export default Home
