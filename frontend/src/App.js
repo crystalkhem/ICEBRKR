453a3e92e11fe7d8d5e0253d75c7900234ce7e29
crystalkhem <47156291+crystalkhem@users.noreply.github.com>
2022-10-21 11:44:34 -0400

    temp static categories added

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> HOMEPAGE</h1>
+      <Home />
+    
+    </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,16 @@
+// import { useEffect, useState } from 'react'
+// import { BrowserRouter as Router, Link } from 'react-router-dom'
+const Home = () => {
+
+    return (
+        <div className="categories">
+            <h2>categories</h2>
+            sports<input type="checkbox" name="layer one" value="sports" /><br />
+            music<input type="checkbox" name="layerone" value="music" /><br />
+            tv shows<input type="checkbox" name="layerone" value="tv shows" /><br />
+            animals<input type="checkbox" name="layerone" value="animals" />
+        </div>
+    )
+}
+
+export default Home
