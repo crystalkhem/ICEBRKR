b3b3c4fe72e7d3287fc5605e545bd03d040fec19
crystalkhem <47156291+crystalkhem@users.noreply.github.com>
2022-11-19 10:43:55 -0500

    removal of registration shit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import { signupUser, loginUser, registerUser } from '../controller/userController.js' 
-// const { signupUser, loginUser, registerUser } = require('../controller/userController')
 const router = express.Router()
 
 //login route
@@ -8,8 +7,5 @@ router.post('/login', loginUser)
 
 // signup route
 router.post('/signup', signupUser)
-
-// post 
-router.post('/register', registerUser)
 
 export default router;
